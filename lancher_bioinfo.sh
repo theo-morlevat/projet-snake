687ca457d996f042e04195420b61f9b0d9b23fb9
theo-morlevat <mistertheo26@gmail.com>
2025-12-14 18:21:42 +0100

    Update lancher_bioinfo.sh

--- lancher_bioinfo.sh
+++ lancher_bioinfo.sh
@@ -1,22 +1,23 @@
 clear #On commence par effacer la console
 
 if [ $1 == --help ] || [ $1 == -h ]; then  #option help, détaille le fonctionnement du script.
-       echo -e " The Bash script will copy the pyhton script at the destination of the .sam file. Then ... "
+       echo -e "Usage: /path/lancher_bioinfo.sh [path/directory] [SAM_FILE]\nAnalyse a sam file to produce fasta file with specifics reads and/ or a summary\n \nListe of options\n -i, --input give informations about input\n -o, --output  give names and types of outputs"
+       exit 1 # arrete l'execution du programme
+fi
+
+if [ $1 == --input ] || [ $1 == -i ]; then  #option input, détaille le fonctionnement du script.
+       echo -e "This command need a first argument, a path wich lead to the directory which contain the sam file.\nThe second argument is the name of your sam file.\nAn exemple: ~/bio-info/lancher_bioinfo.sh ~/my_experiments/exp_1 my_alignement.sam"
        exit 1
 fi
 
-if [ $1 == --output ] || [ $1 == -o ]; then  #option help, détaille le fonctionnement du script.
-       echo -e " The Bash script will copy the pyhton script at the destination of the .sam file. Then ... "
+if [ $1 == --output ] || [ $1 == -o ]; then  #option output, donne une explication des données d'entrée et un exemple.
+       echo -e "This programme can output 4 differents files:\nsummary.txt : synthetic rapport\nunmapped.fasta : sequencies of unmapped reads\npartiallyMapped.fasta : sequencies of partially mapped reads\nmapped.fasta : sequencies of corrected mapped reads"
        exit 1
 fi
 
-if [ $1 == --input ] || [ $1 == -i ]; then  #option help, détaille le fonctionnement du script.
-       echo -e " The Bash script will copy the pyhton script at the destination of the .sam file. Then ... "
-       exit 1
-fi
-
-cp analyse_sam.py "$1"analyse_sam2.py # Le script .py se situe dans le même fichier que le script actuel. On copie le script .py dans dans le dossier où se situe le fichier sam. Le script est copié sous un autre nom si le fichier sam se trouve dans le même dossier pour éviter de supprimer le fichier .py d'origine.
+cp analyse_sam.py "$1"analyse_sam2.py #Le script .py se situe dans le même fichier que le script actuel. On copie le script .py dans dans le dossier où se situe le fichier sam. Le script est copié sous un autre nom si le fichier sam se trouve dans le même dossier pour éviter de supprimer le fichier .py d'origine.
 cd "$1" #On déplace ensuite l'endroit d'exécution des actions dans le dossier contenant le fichier .sam. Ainsi on pourra exploiter le fichier avec le script .py.
+
 
 error="" #On initialise une variable vide afin de contenir les différents messages d'erreurs.
 last-extension="${2##*.}" #On stock la dernière extension du nom du fichier dans la variable last-extension
@@ -24,56 +25,30 @@ last-extension="${2##*.}" #On stock la dernière extension du nom du fichier dan
 
 if [ -z "$2" ] || [ ! -e "$2" ] || [ ! -f "$2" ] || [[ ! -s "$2" ]] || [ $last-extension != sam ]; then #On vérifie s'il n'y a pas d'érreur du à: un argument manquant, un fichier absent, un argument ne correspondant pas à un fichier régulier, un fichier vide ou n'ayant pas l'extension .sam.
         [ -z "$2" ]   &&  error+="Error: nom de fichier manquant.\n"  # && Et un opérateur permettant de lancer une commande que si la commande précédante s'exécute
-        [ ! -e "$2" ] &&  error+="Error: $2 n'est pas présent dans le dossier.\n"
+        [ ! -e "$2" ] &&  error+="Error: $2 n'est pas présent dans le dossier.\n" #on affiche alors un message en lien avec l'erreur
         [ ! -f "$2" ] &&  error+="Error: $2 n'est pas un fichier régulier.\n"
         [ ! -s "$2" ] &&  error+="Error: $2 est vide.\n"
         [[ $last-extension != sam ]] && error+="Error: $2 n'est pas un fichier sam."
         echo -e "$error" #On affiche les différents erreurs liées à l'exécution de la commande.
         exit 1 #Si il y a une erreur alors le programme s'arrête
 else
-        echo "Les vérifications sont terminées" #Sinon on averti l'utilisateur que l'on va pouvoir travailler sur le fichier.
+        echo "Verifications completed" #Sinon on averti l'utilisateur que l'on va pouvoir travailler sur le fichier.
 fi
 
 
-#read = lire.clavier
-echo "=== Analyseur de Fichiers SAM ===
-1) Analyse COMPLÈTE
-2) Analyse CIGAR
-3) Analyse MAPPING
-4) Extraction FASTA
-Choisissez une option [1-4] : 1
+echo -e "===  SAM File Analyseur ===\n1) Complete Analyse\n2) CIGAR Analyse\n3) MAPPING Analyse\n4) FASTA Extraction\nChoose an option [1-4]\nAnswer in the console" # affiche un message demandant le type d'analyse à réaliser
+read typeofanalyse # l'utilisateur entre la valeur dans la console, la valeur est stocké dans la variable typeofanalyse
 
-Analyse en cours... (exp_1/mon_alignement.sam)
-✓ Analyse terminée avec succès
-Fichiers générés : summary.txt, unmapped.fasta, partiallyMapped.fasta"
-read answer 
 
-if [ $all == "1" ] || [ 
+if [ $typeofanalyse == 1 ] || [ $typeofanalyse == 2 ] || [ $typeofanalyse == 3 ] || [ $typeofanalyse == 4 ]; # Si la variable correspond aux paramètres attendus
 then
-          python3 script2.py $2
+          echo "Analysis in progress..." # on affiche que l'analyse est en cours
+          python3 analyse_sam2.py $2 $typeofanalyse # on execute le script python 
+          echo -e "Analysis successfully completed\nFiles have been generated in the selected path." # Une fois l'analyse fini on affiche un message de validation
 else
-          param=""
-          echo "You want to work on: 'mapped-reads', 'partially-mapped-reads', 'unmapped-reads', 'all'? "
-          read mapped
-          if [[ $mapped == "mapped-reads" ]] || [[ $mapped == "partially-mapped-reads" ]] || [[ $mapped == "unmapped-reads" ]] || [[ $mapped ==  "all" ]]
-          then
-            param+="$mapped"
-          else
-            echo "Error: you didn't write well parameters"
+            echo "Error: you didn't write well parameters" # si les paramètres ne sont pas correct on affiche un message d'erreur
             exit 1
-          fi
-          echo " You want to work on: 'reads-number' or 'percentage'? "
-          read type
-          if [[ $type == "reads-number" ]] || [[ $type == percentage ]]
-          then
-            param+="$mapped"
-          else
-            echo "Error: you didn't write well parameters"
-            exit 1
-          fi
-          echo "$param"
-
 fi
 
 
-rm script2.py
+rm analyse_sam2.py # On supprime le script du dossier contenant les données.
